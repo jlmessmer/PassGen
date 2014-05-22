e625d2724a4efd69340f520393e058ab9853467b
liteblade <jimmymessmer@Smacbook.local>
2014-05-22 14:07:54 -0400

    Added Worker Class
    Adding a worker class to try to implement multithreading the
    application.

--- src/PassGenRunner.java
+++ src/PassGenRunner.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class PassGen {
+public class PassGenRunner {
 	
 	private static BufferedReader in;
 
---
+++ src/PassGenWorker.java
@@ -0,0 +1,12 @@
+
+public class PassGenWorker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
